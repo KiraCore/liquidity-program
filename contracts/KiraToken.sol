475ae2c0317e6199cf84645c83958cf636dcd295
James Truong <trueeyedev@gmail.com>
2020-09-15 19:42:46 +1000

    added whitelistAdd, whitelistRemove, _beforeTokenTransfer added test suites

--- contracts/KiraToken.sol
+++ contracts/KiraToken.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.6.2;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/access/Ownable.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/contracts/access/Ownable.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 /**
  * @title KiraToken
@@ -13,16 +13,21 @@ contract KiraToken is ERC20, Ownable {
     using SafeMath for uint256;
 
     // modify token name
-    string public constant NAME = "KIRA Network";
+    string public constant NAME = 'KIRA Network';
     // modify token symbol
-    string public constant SYMBOL = "KEX";
+    string public constant SYMBOL = 'KEX';
     // modify token decimals
     uint8 public constant DECIMALS = 6;
     // modify initial token supply
-    uint256 public constant INITIAL_SUPPLY = 300000000 *
-        (10**uint256(DECIMALS)); // 10000 tokens
+    uint256 public constant INITIAL_SUPPLY = 300000000 * (10**uint256(DECIMALS)); // 10000 tokens
 
-    bool public freezed;
+    bool public freezed; // indicate if the token is freezed or not
+
+    mapping(address => bool) private _whitelist; // represents if the address is whitelisted or not
+
+    // Events
+    event AddressWhitelisted(address addr);
+    event AddressWhitelistRemoved(address addr);
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
@@ -32,15 +37,56 @@ contract KiraToken is ERC20, Ownable {
         _mint(msg.sender, INITIAL_SUPPLY);
         emit Transfer(address(0x0), msg.sender, INITIAL_SUPPLY);
         freezed = true;
+        _whitelist[msg.sender] = true;
     }
 
     function freeze() external onlyOwner {
-        require(freezed == false, "KEX: already freezed");
+        require(freezed == false, 'KEX: already freezed');
         freezed = true;
     }
 
     function unfreeze() external onlyOwner {
-        require(freezed == true, "KEX: already unfreezed");
+        require(freezed == true, 'KEX: already unfreezed');
         freezed = false;
+    }
+
+    // Add to whitelist
+
+    function whitelistAdd(address addr) external onlyOwner returns (bool) {
+        // require(
+        //     _whitelist[addr] == false,
+        //     "KEX: the address is already whitelisted"
+        // );
+        _whitelist[addr] = true;
+        emit AddressWhitelisted(addr);
+        return true;
+    }
+
+    // Remove from whitelist
+
+    function whitelistRemove(address addr) external onlyOwner returns (bool) {
+        require(addr != owner(), "KEX: can not remove owner's whitelist");
+        require(_whitelist[addr] == true, 'KEX: the address is not whitelisted');
+        _whitelist[addr] = false;
+        emit AddressWhitelistRemoved(addr);
+        return true;
+    }
+
+    /**
+     * @dev Returns if the address is whitelisted or not.
+     */
+    function whitelisted(address addr) public view returns (bool) {
+        return _whitelist[addr];
+    }
+
+    // TOKEN TRANSFER HOOK
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+        require(!freezed || (_whitelist[from] && _whitelist[to]), 'KEX: token transfer while freezed and not whitelisted.');
     }
 }
--- test/KiraToken.test.js
+++ test/KiraToken.test.js
@@ -11,49 +11,68 @@ chai.use(chaiAsPromised)
 
 const expect = chai.expect
 
-contract('KiraToken Test', async (accounts) => {
+contract('KiraToken Test', async function (accounts) {
   const [deployerAccount, recipient] = accounts
 
-  it('all tokens should be in my account', async () => {
-    let instance = await Token.deployed()
-    let totalSupply = await instance.totalSupply()
-    expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply)
+  describe('totalSupply', function () {
+    it('all tokens should be in my account', async function () {
+      let instance = await Token.deployed()
+      let totalSupply = await instance.totalSupply()
+      await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply)
+    })
   })
 
-  it('should be freezed at first', async () => {
-    let instance = await Token.deployed()
-    const freezed = await instance.freezed()
-    expect(freezed).to.equal(true)
+  describe('freeze', function () {
+    it('should be freezed at first and the transfer should be rejected', async function () {
+      let instance = await Token.deployed()
+      const freezed = await instance.freezed()
+      expect(freezed).to.equal(true)
+
+      const sendTokens = 100
+      await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.rejected
+    })
   })
 
-  // it('is possible to send tokens between accounts when paused', async () => {
-  //   const sendTokens = 100
-  //   let instance = await Token.deployed()
-  //   let totalSupply = await instance.totalSupply()
-  //   expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply)
-  //   expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled
-  //   expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)))
-  //   expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens))
-  // })
+  describe('unfreeze', function () {
+    it('should be able to transfer freely once unfreezed', async function () {
+      let instance = await Token.deployed()
+      await expect(instance.unfreeze()).to.eventually.be.fulfilled
 
-  // it('is possible to send tokens between accounts when unpaused', async () => {
-  //   const sendTokens = 100
-  //   let instance = await Token.deployed()
-  //   let totalSupply = await instance.totalSupply()
-  //   await instance.unpause()
-  //   expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply)
-  //   expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled
-  //   expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)))
-  //   expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens - (sendTokens * 4) / 100))
-  //   expect(instance.rewardPool()).to.eventually.be.a.bignumber.equal(new BN((sendTokens * 2) / 100))
-  // })
+      const freezed = await instance.freezed()
+      expect(freezed).to.equal(false)
 
-  // it('is not possible to send more tokens than available in total', async () => {
-  //   let instance = await Token.deployed()
-  //   let balanceOfDeployer = await instance.balanceOf(deployerAccount)
+      const sendTokens = 100
+      const oldFromBalance = await instance.balanceOf(deployerAccount)
+      const oldToBalance = await instance.balanceOf(recipient)
+      await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled
+      await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(oldFromBalance.sub(new BN(sendTokens)))
+      await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(oldToBalance.add(new BN(sendTokens)))
+    })
+  })
 
-  //   expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected
+  describe('whitelist', function () {
+    it('should be able to transfer between whitelisted accounts even if the token is freezed', async function () {
+      let instance = await Token.deployed()
+      await expect(instance.freeze()).to.eventually.be.fulfilled
 
-  //   expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber(balanceOfDeployer)
-  // })
+      const freezed = await instance.freezed()
+      expect(freezed).to.equal(true)
+
+      await expect(instance.whitelistAdd(recipient)).to.eventually.be.fulfilled
+
+      const sendTokens = 100
+      const oldFromBalance = await instance.balanceOf(deployerAccount)
+      const oldToBalance = await instance.balanceOf(recipient)
+      await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled
+      await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(oldFromBalance.sub(new BN(sendTokens)))
+      await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(oldToBalance.add(new BN(sendTokens)))
+    })
+  })
+
+  describe('whitelistRemove', async function (done) {
+    it('should not be able to remove owner from whitelist', async function () {
+      let instance = await Token.deployed()
+      await expect(instance.whitelistRemove(deployerAccount)).to.eventually.be.rejected
+    })
+  })
 })

28874cfd9a28ea810789dfaac2083ff5d0e7a7e1
asmodat <asmodat@gmail.com>
2020-11-23 22:53:21 +0000

    Get kex balance

--- LIP_3.1/src/hooks/useTokenBalance.ts
+++ LIP_3.1/src/hooks/useTokenBalance.ts
@@ -1,14 +1,32 @@
+import { useCallback, useEffect, useState } from 'react'
+
 import BigNumber from 'bignumber.js'
+import { useWallet } from 'use-wallet'
+import { provider } from 'web3-core'
+import { getBalance } from '../utils/kira'
 import cfgData from '../config.json';
 
 const useTokenBalance = () => {
-    const resCnf: any = cfgData; // Config Data
+  const resCnf: any = cfgData; // Config Data
+  const kexAddress = resCnf['token'];
+  const [balance, setBalance] = useState(new BigNumber(0))
+  const {
+    account,
+    ethereum,
+  }: { account: string; ethereum: provider } = useWallet()
 
-    if (!resCnf) {
-      throw new Error("ERROR: Can't fetch Configuration Data");
+  const fetchBalance = useCallback(async () => {
+    const balance = await getBalance(ethereum, kexAddress, account)
+    setBalance(new BigNumber(balance))
+  }, [account, ethereum, kexAddress])
+
+  useEffect(() => {
+    if (account && ethereum) {
+      fetchBalance()
     }
+  }, [account, ethereum, setBalance, kexAddress])
 
-    return new BigNumber(resCnf["available"]); // KEX available for distribution
+  return balance
 }
 
 export default useTokenBalance

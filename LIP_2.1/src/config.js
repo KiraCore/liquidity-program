44fb3d6bb9cafafccc13e67854451f3850d8abbf
Ryan Choi <choi.link227@gmail.com>
2020-12-04 10:01:49 -0500

    update config.js

--- LIP_2.1/src/config.js
+++ LIP_2.1/src/config.js
@@ -1,13 +1,12 @@
 require('dotenv/config')
 
-console.log(process.env.REACT_APP_KEX_CONTRACT)
 const config = {
     NETWORK: process.env.ENVIRONMENT === "production" ? "mainnet" : "kovan",
     KEX_CONTRACT: process.env.REACT_APP_KEX_CONTRACT || '0x539fa9544ea8F82A701b6d3c6A6F0e2ebE307eA6',
     LOCKING_CONTRACT: process.env.REACT_APP_LOCKING_CONTRACT || '0x358fdD1D21CD86275F77990C5939447C1Aed3A4E',
     LP_CONTRACT: process.env.REACT_APP_LP_CONTRACT || '0x5ed9cf25c542c19005c6096acbf6463771e13ff1',
     WETH_CONTRACT: process.env.REACT_APP_WETH_CONTRACT || '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
-    INFURA_PROJECT_ID: process.env.REACT_APP_INFURA_PROJECT_ID || '23988ae61b6d4e3f851fce20720dae12'
+    INFURA_PROJECT_ID: process.env.REACT_APP_INFURA_PROJECT_ID
 }
 
 export default config;

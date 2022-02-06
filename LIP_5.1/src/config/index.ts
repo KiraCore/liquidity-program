7e2ce84d328911b05c0ddf2f28a7d697c87941c1
asmodat <asmodat@gmail.com>
2022-02-06 12:12:22 +0000

    cast all envs to numbers or strings

--- LIP_5.1/src/config/index.ts
+++ LIP_5.1/src/config/index.ts
@@ -1,6 +1,6 @@
-export const ETHEREUM_RPC_URL = process.env.REACT_APP_ETHEREUM_RPC_URL;
+export const ETHEREUM_RPC_URL = String(process.env.REACT_APP_ETHEREUM_RPC_URL);
 export const ETHEREUM_CHAIN_ID = Number(process.env.REACT_APP_ETHEREUM_CHAIN_ID);
-export const KIRA_TOKEN_ADDRESS = process.env.REACT_APP_KIRA_TOKEN_ADDRESS;
-export const NFT_MINTING_ADDRESS = process.env.REACT_APP_NFT_MINTING_ADDRESS;
-export const NFT_FARM_ADDRESS = process.env.REACT_APP_NFT_FARM_ADDRESS;
-export const NFT_STAKING_ADDRESS = process.env.REACT_APP_NFT_STAKING_ADDRESS;
+export const KIRA_TOKEN_ADDRESS = String(process.env.REACT_APP_KIRA_TOKEN_ADDRESS);
+export const NFT_MINTING_ADDRESS = String(process.env.REACT_APP_NFT_MINTING_ADDRESS);
+export const NFT_FARM_ADDRESS = String(process.env.REACT_APP_NFT_FARM_ADDRESS);
+export const NFT_STAKING_ADDRESS = String(process.env.REACT_APP_NFT_STAKING_ADDRESS);

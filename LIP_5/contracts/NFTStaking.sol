be0e80f486ef702a582b39ea5bd56c68c033eda1
asmodat <asmodat@gmail.com>
2022-03-27 12:53:19 +0000

    set correct subdirectory

--- LIP_5/contracts/NFTStaking.sol
+++ LIP_5/contracts/NFTStaking.sol
@@ -6,7 +6,7 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC1155/IERC1155.sol';
 import '@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 struct STAKE {
     uint256 amount;
